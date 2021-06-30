dffab2118bd8fff21fa3a51a701f987ed82e7647
NCJ Charlie <me@ncj.wiki>
2021-07-01 01:36:44 +0800

    finished addMedicalRecord Page

--- src/pages/DoctorPage/addMedicalRecord.tsx
+++ src/pages/DoctorPage/addMedicalRecord.tsx
@@ -1,30 +1,15 @@
 // @ts-ignore
 import React from 'react';
-import ProForm, {
-  StepsForm,
-  ProFormText,
-  ProFormDatePicker,
-  ProFormDateRangePicker,
-  ProFormSelect,
-  ProFormCheckbox,
-  ProFormDigit,
-} from '@ant-design/pro-form';
+import { StepsForm, ProFormTextArea } from '@ant-design/pro-form';
 import { history } from 'umi';
 import ProCard from '@ant-design/pro-card';
 import { Card, message } from 'antd';
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
 import { useIntl } from '@@/plugin-locale/localeExports';
 import ChatUI from '@/components/ChatUI/ChatUI';
+import { AddMedicalRecord } from '@/services/MSaaS/medicalRecords';
 
-const waitTime = (time: number = 100) => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(true);
-    }, time);
-  });
-};
-
-export default () => {
+export default (props: { location: { query: { appointmentId: any } } }) => {
   const intl = useIntl();
   return (
     <PageHeaderWrapper
@@ -44,7 +29,7 @@ export default () => {
             maxWidth: '450px',
           }}
         >
-          <ChatUI />
+          <ChatUI appointmentId={props.location.query.appointmentId} />
         </Card>
         <Card
           title="诊断结果"
@@ -52,10 +37,16 @@ export default () => {
         >
           <StepsForm
             onFinish={async (values) => {
-              console.log(values);
-              await waitTime(1000);
-              message.success('提交成功');
-              history.goBack();
+              const res = await AddMedicalRecord({
+                ...values,
+                appointmentId: props.location.query.appointmentId,
+              });
+              if (res.id) {
+                message.success('提交成功');
+                history.goBack();
+              } else {
+                message.error('提交失败');
+              }
             }}
             formProps={{
               validateMessages: {
@@ -63,16 +54,9 @@ export default () => {
               },
             }}
           >
-            <StepsForm.StepForm
-              name="base"
-              title="第一步骤"
-              onFinish={async () => {
-                await waitTime(2000);
-                return true;
-              }}
-            >
+            <StepsForm.StepForm name="base" title="基本信息">
               <ProCard
-                title="源和目标"
+                title="既往病史"
                 bordered
                 headerBordered
                 style={{
@@ -81,22 +65,16 @@ export default () => {
                   maxWidth: '100%',
                 }}
               >
-                <ProFormText
-                  name="name"
-                  width="md"
-                  label="迁移任务名称"
-                  tooltip="最长为 24 位，用于标定的唯一 id"
-                  placeholder="请输入名称"
+                <ProFormTextArea
+                  name="pastMedicalHistory"
+                  label="既往病史"
+                  placeholder="请输入"
                   rules={[{ required: true }]}
                 />
-                <ProForm.Group title="节点" size={8}>
-                  <ProFormSelect width="sm" name="source" placeholder="选择来源节点" />
-                  <ProFormSelect width="sm" name="target" placeholder="选择目标节点" />
-                </ProForm.Group>
               </ProCard>
 
               <ProCard
-                title="顶部对齐"
+                title="症状描述"
                 bordered
                 headerBordered
                 style={{
@@ -104,104 +82,49 @@ export default () => {
                   marginBottom: 16,
                 }}
               >
-                <ProFormDigit
-                  name="xs"
-                  label="XS号表单"
-                  initialValue={9999}
-                  tooltip="悬浮出现的气泡。"
-                  placeholder="请输入名称"
-                  width="xs"
-                />
-                <ProFormText name="s" label="S号表单" placeholder="请输入名称" width="sm" />
-                <ProFormDateRangePicker name="m" label="M 号表单" />
-                <ProFormSelect
-                  name="l"
-                  label="L 号表单"
-                  fieldProps={{
-                    mode: 'tags',
-                  }}
-                  width="lg"
-                  initialValue={['吴家豪', '周星星']}
-                  options={['吴家豪', '周星星', '陈拉风'].map((item) => ({
-                    label: item,
-                    value: item,
-                  }))}
+                <ProFormTextArea
+                  name="symptom"
+                  label="症状"
+                  placeholder="请输入"
+                  rules={[{ required: true }]}
                 />
               </ProCard>
             </StepsForm.StepForm>
-            <StepsForm.StepForm name="checkbox" title="第二步骤">
+            <StepsForm.StepForm name="diagnosis" title="诊断结果">
               <ProCard
-                style={{
-                  minWidth: 800,
-                  marginBottom: 16,
-                  maxWidth: '100%',
-                }}
-              >
-                <ProFormCheckbox.Group
-                  name="checkbox"
-                  label="迁移类型"
-                  width="lg"
-                  options={['结构迁移', '全量迁移', '增量迁移', '全量校验']}
-                />
-                <ProForm.Group>
-                  <ProFormText name="dbname" label="业务 DB 用户名" />
-                  <ProFormDatePicker name="datetime" label="记录保存时间" width="sm" />
-                </ProForm.Group>
-                <ProFormCheckbox.Group
-                  name="checkbox"
-                  label="迁移类型"
-                  options={['完整 LOB', '不同步 LOB', '受限制 LOB']}
-                />
-              </ProCard>
-            </StepsForm.StepForm>
-            <StepsForm.StepForm name="time" title="第三步骤">
-              <ProCard
+                title="诊断结果"
+                bordered
+                headerBordered
                 style={{
                   marginBottom: 16,
                   minWidth: 800,
                   maxWidth: '100%',
                 }}
               >
-                <ProFormCheckbox.Group
-                  name="checkbox"
-                  label="部署单元"
-                  rules={[
-                    {
-                      required: true,
-                    },
-                  ]}
-                  options={['部署单元1', '部署单元2', '部署单元3']}
+                <ProFormTextArea
+                  name="diagnosis"
+                  label="诊断结果"
+                  placeholder="请输入"
+                  rules={[{ required: true }]}
                 />
-                <ProFormSelect
-                  label="部署分组策略"
-                  name="remark"
-                  rules={[
-                    {
-                      required: true,
-                    },
-                  ]}
-                  width="md"
-                  initialValue="1"
-                  options={[
-                    {
-                      value: '1',
-                      label: '策略一',
-                    },
-                    { value: '2', label: '策略二' },
-                  ]}
-                />
-                <ProFormSelect
-                  label="Pod 调度策略"
-                  name="remark2"
-                  width="md"
-                  initialValue="2"
-                  options={[
-                    {
-                      value: '1',
-                      label: '策略一',
-                    },
-                    { value: '2', label: '策略二' },
-                  ]}
+              </ProCard>
+            </StepsForm.StepForm>
+            <StepsForm.StepForm name="prescription" title="开具处方">
+              <ProCard
+                title="处方与医嘱"
+                bordered
+                headerBordered
+                style={{
+                  marginBottom: 16,
+                  minWidth: 800,
+                  maxWidth: '100%',
+                }}
+              >
+                <ProFormTextArea
+                  name="prescription"
+                  label="处方与医嘱"
+                  placeholder="请输入"
+                  rules={[{ required: true }]}
                 />
               </ProCard>
             </StepsForm.StepForm>
