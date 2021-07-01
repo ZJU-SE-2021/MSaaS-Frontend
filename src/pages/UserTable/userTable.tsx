f43094ccf872d82101a5849e368c9c1d9e9b46bd
NCJ Charlie <me@ncj.wiki>
2021-07-01 20:48:45 +0800

    sort, filter, search on users

--- src/pages/UserTable/userTable.tsx
+++ src/pages/UserTable/userTable.tsx
@@ -1,12 +1,11 @@
 import { PageHeaderWrapper } from '@ant-design/pro-layout';
-import {useIntl} from 'umi';
+import { useIntl } from 'umi';
 import React, { useRef } from 'react';
 import { PlusOutlined } from '@ant-design/icons';
-import {Button, Card} from 'antd';
+import { Button, Card } from 'antd';
 import type { ProColumns, ActionType } from '@ant-design/pro-table';
 import ProTable from '@ant-design/pro-table';
-import {GetUsers} from "@/services/MSaaS/users";
-
+import { GetUsers } from '@/services/MSaaS/users';
 
 const columns: ProColumns<API.UserDto>[] = [
   {
@@ -47,20 +46,18 @@ const columns: ProColumns<API.UserDto>[] = [
     title: '性别',
     dataIndex: 'gender',
     filters: true,
-    // onFilter: true,
     hideInSearch: true,
     valueType: 'select',
     valueEnum: {
-      all: { text: '全部', status: 'Default' },
-      male: { text: '男', status: 'Male' },
-      female: { text: '女', status: 'Female' }
+      Male: { text: '男' },
+      Female: { text: '女' },
+      Other: { text: '其他' },
     },
   },
   {
     title: '生日',
     dataIndex: 'birthday',
     valueType: 'date',
-    hideInSearch: true,
     sorter: true,
   },
   {
@@ -69,19 +66,16 @@ const columns: ProColumns<API.UserDto>[] = [
     valueType: 'digit',
     hideInSearch: true,
     editable: false,
-    // sorter: true,
   },
   {
     title: '电话',
     dataIndex: 'phone',
     valueType: 'text',
-    // sorter: true,
   },
   {
     title: '邮箱',
     dataIndex: 'email',
     valueType: 'text',
-    // sorter: true,
   },
   {
     title: '操作',
@@ -115,14 +109,51 @@ export default (): React.ReactNode => {
         <ProTable<API.UserDto>
           columns={columns}
           actionRef={actionRef}
-          // request={GetUsers}
-          request={async (params,sort,filter) => {
-            const data = await GetUsers()
-            console.log(params, sort, filter)
+          request={async (params, sort, filter) => {
+            let data = await GetUsers();
+            data = data.filter((user) => {
+              return (
+                (params.birthday === undefined ||
+                  (user.birthday && user.birthday.slice(0, 10) === params.birthday)) &&
+                (params.name === undefined || (user.name && user.name.includes(params.name))) &&
+                (params.username === undefined ||
+                  (user.username && user.username.includes(params.username))) &&
+                (params.email === undefined || (user.email && user.email.includes(params.email))) &&
+                (params.phone === undefined || (user.phone && user.phone.includes(params.phone))) &&
+                (filter.gender === undefined ||
+                  filter.gender === null ||
+                  (user.gender && filter.gender.includes(user.gender)))
+              );
+            });
+            const total = data.length;
+            if (sort !== undefined) {
+              switch (sort.time) {
+                case 'ascend':
+                  data.sort((a, b) => {
+                    // @ts-ignore
+                    return new Date(a.time) - new Date(b.time);
+                  });
+                  break;
+                case 'descend':
+                  data.sort((a, b) => {
+                    // @ts-ignore
+                    return new Date(b.time) - new Date(a.time);
+                  });
+                  break;
+                default:
+                // do nothing
+              }
+            }
+            if (params.current && params.pageSize)
+              data = data.slice(
+                (params.current - 1) * params.pageSize,
+                params.current * params.pageSize,
+              );
             return {
               data,
-              success: true
-            }
+              total,
+              success: true,
+            };
           }}
           editable={{
             type: 'multiple',
@@ -151,7 +182,7 @@ export default (): React.ReactNode => {
           toolBarRender={() => [
             <Button key="button" icon={<PlusOutlined />} type="primary">
               新建
-            </Button>
+            </Button>,
           ]}
         />
       </Card>
